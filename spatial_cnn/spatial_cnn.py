43a071b0561674ced671a20e6ba1f0be7bb7c89b
DESKTOP-GBTQ6GM\jeffrey <jeffreyhuang0823@gmail.com>
2017-08-01 14:18:35 +0800

    add default value of hyper parameter

--- spatial_cnn/spatial_cnn.py
+++ spatial_cnn/spatial_cnn.py
@@ -5,6 +5,7 @@ import time
 from tqdm import tqdm
 import pandas as pd
 import shutil
+import argparse
 import lr_scheduler
 
 from torch.utils.data import Dataset, DataLoader
@@ -18,6 +19,133 @@ from torch.optim import Optimizer
 
 torch.cuda.set_device(0)
 print '\nCurrent Device',torch.cuda.current_device(),' Available:',torch.cuda.is_available()
+
+# Default value
+parser = argparse.ArgumentParser(description='PyTorch UCF101 spatial stream training')
+parser.add_argument('--epochs', default=50, type=int, metavar='N', help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N', help='manual epoch number (useful on restarts)')
+parser.add_argument('--batch-size', default=64, type=int, metavar='N', help='mini-batch size (default: 256)')
+parser.add_argument('--lr', default=1e-4, type=float, metavar='LR', help='initial learning rate')
+parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
+parser.add_argument('--evaluate', dest='evaluate', action='store_true', help='evaluate model on validation set')
+
+best_prec1 = 0
+
+def main():
+
+    global args, best_prec1
+    args = parser.parse_args()
+
+    # Hyper Parameters
+    EPOCH = args.epochs         
+    BATCH_SIZE = args.batch_size
+    LR = args.lr 
+    print '==> Create Model'
+    #Create Model
+    Model = models.resnet101(pretrained=True)
+      #Replace fc1000 with fc101
+    num_ftrs = Model.fc.in_features
+    Model.fc = nn.Linear(num_ftrs, 101)
+      #convert model to gpu
+    Model = Model.cuda()
+
+    #Loss function and optimizer
+    criterion = nn.CrossEntropyLoss().cuda()
+
+    optimizer = torch.optim.SGD(Model.parameters(), LR, momentum=0.9)
+    scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=0,verbose=True)
+
+    # optionally resume from a checkpoint
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("==> loading checkpoint '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            best_prec1 = checkpoint['best_prec1']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("==> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+        else:
+            print("==> no checkpoint found at '{}'".format(args.resume))
+    
+    print '==> Preparing training and validation data'
+    #load dictionary
+    dic_training, dic_testing = load_dic()
+
+    # change DIC -> LIST format due to time cost
+    L_train_keys = dic_training.keys()
+    L_train_values = dic_training.values()
+
+    L_test_keys = dic_testing.keys()
+    L_test_values = dic_testing.values()
+       
+    #Build training & testing set
+    training_set = UCF101_rgb_data(keys=L_train_keys, values=L_train_values, root_dir='/store/ucf101/spatial/',transform = transforms.Compose([
+                transforms.Scale(256),
+                transforms.RandomCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
+                ]))
+    
+    testing_set = UCF101_rgb_data(keys=L_test_keys, values=L_test_values, root_dir='/store/ucf101/spatial/',transform = transforms.Compose([
+                transforms.Scale(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
+                ]))
+    
+    #Data Loader
+    train_loader = DataLoader(
+        dataset=training_set, 
+        batch_size=BATCH_SIZE,
+        shuffle=True,
+        num_workers=8
+        )
+    
+    test_loader = DataLoader(
+        dataset=testing_set, 
+        batch_size=BATCH_SIZE, 
+        shuffle=False,
+        num_workers=8
+        )
+    
+    cudnn.benchmark = True
+    # Evaluation mode
+    if args.evaluate:
+        validate(test_loader, Model, criterion,L_test_keys, BATCH_SIZE)
+        return
+
+    print '==> Start training'
+    #Training & testing
+    for epoch in range(args.start_epoch, EPOCH):
+
+        # train for one epoch
+        print(' Epoch:[{0}/{1}][training stage]'.format(epoch, EPOCH))
+        train(train_loader, Model, criterion, optimizer, epoch)
+
+        # evaluate on validation set
+        print(' Epoch:[{0}/{1}][validation stage]'.format(epoch, EPOCH))
+        prec1, val_loss, dic_video_level_preds = validate(test_loader, Model, criterion, 0, L_test_keys,BATCH_SIZE)
+        
+        #Call lr_scheduler
+        scheduler.step(val_loss)
+
+        #Calculate Video level acc
+        top1,top5 = video_level_acc(dic_video_level_preds)
+
+        # remember best prec@1 and save checkpoint
+        is_best = prec1 > best_prec1
+        best_prec1 = max(prec1, best_prec1)
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'arch': 'resnet101',
+            'state_dict': Model.state_dict(),
+            'best_prec1': best_prec1,
+            'optimizer' : optimizer.state_dict(),
+        }, is_best,dic_video_level_preds)
+
 
 def load_dic():
 
@@ -60,52 +188,6 @@ class UCF101_rgb_data(Dataset):
                  
         img.close()
         return sample
-
-def video_level_acc(dic_video_level_preds):
-    
-    with open('/home/jeffrey/pytorch/ucf101_two_stream/dic_video_label.pickle','rb') as f:
-        dic_video_label = pickle.load(f)
-    f.close()
-    
-    correct = 0
-    video_level_preds = np.zeros((len(dic_video_level_preds),101))
-    video_level_labels = np.zeros(len(dic_video_level_preds))
-    ii=0
-    for line in sorted(dic_video_level_preds.keys()):
-        preds = dic_video_level_preds[line]
-        label = int(dic_video_label[line])-1
-        
-        video_level_preds[ii,:] = preds
-        video_level_labels[ii] = label
-        ii+=1 
-        
-        if np.argmax(preds) == (label):
-            correct+=1
-            
-    #print video_level_labels, video_level_preds
-    #top1 top5
-    video_level_labels = torch.from_numpy(video_level_labels).long()
-    video_level_preds = torch.from_numpy(video_level_preds).float()
-    
-    top1,top5 = accuracy(video_level_preds, video_level_labels, topk=(1,5))     
-                    
-    #top1 = float(correct)/float(len(dic_video_level_preds))
-    top1 = float(top1.numpy())
-    top5 = float(top5.numpy())
-   
-
-    #record as csv file
-    filename = 'record/video_level_acc.csv'
-    column_names = ['Prec@1','Prec@5']
-    df =pd.DataFrame({'Prec@1':[top1],'Prec@5':[top5]})
-
-    if not os.path.isfile(filename):
-        df.to_csv(filename,index=False,columns=column_names)
-    else: # else it exists so append without writing the header
-        df.to_csv(filename,mode = 'a',header=False,index=False,columns=column_names)
-    
-    print(' * Video level Prec@1 {top1:.3f}, Video level Prec@5 {top5:.3f}'.format(top1=top1, top5=top5))
-    return top1,top5
     
 def train(train_loader, Model, criterion, optimizer, epoch):
     
@@ -192,7 +274,7 @@ def train(train_loader, Model, criterion, optimizer, epoch):
             else: # else it exists so append without writing the header
                 df.to_csv(filename,mode = 'a',header=False,index=False,columns=column_names) 
             
-def validate(test_loader, Model, criterion, epoch, L_test_keys, BATCH_SIZE):
+def validate(test_loader, Model, criterion, L_test_keys, BATCH_SIZE):
     
     dic_video_level_preds = {}
     L_video=[]
@@ -247,11 +329,11 @@ def validate(test_loader, Model, criterion, epoch, L_test_keys, BATCH_SIZE):
         ii=i+1
         if ii % (len(test_loader)/5) == 0:
             # Show info on console
-            result = ('Epoch: [{0}], Testing[{1}/{2}]\t'
+            result = (' Testing[{0}/{1}]\t'
                       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                       'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
                       'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                      'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(epoch,
+                      'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
                        i, len(test_loader), batch_time=batch_time, loss=losses,
                        top1=top1, top5=top5))
             print result
@@ -289,110 +371,50 @@ def validate(test_loader, Model, criterion, epoch, L_test_keys, BATCH_SIZE):
 
     return top1.avg, losses.avg, dic_video_level_preds
 
-
-
-
-def main():
-    # Hyper Parameters
-    EPOCH = 50            # train the training data n times, to save time, we just train 1 epoch
-    BATCH_SIZE = 64
-    LR = 5e-3             # learning rate
-
-    #load dictionary
-    dic_training, dic_testing = load_dic()
-
-    # change DIC -> LIST format due to time cost
-    L_train_keys = dic_training.keys()
-    L_train_values = dic_training.values()
-
-    L_test_keys = dic_testing.keys()
-    L_test_values = dic_testing.values()
-       
-    #Build training & testing set
-    training_set = UCF101_rgb_data(keys=L_train_keys, values=L_train_values, root_dir='/store/ucf101/spatial/',transform = transforms.Compose([
-                transforms.Scale(256),
-                transforms.RandomCrop(224),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
-                ]))
+def video_level_acc(dic_video_level_preds):
     
-    testing_set = UCF101_rgb_data(keys=L_test_keys, values=L_test_values, root_dir='/store/ucf101/spatial/',transform = transforms.Compose([
-                transforms.Scale(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
-                ]))
+    with open('/home/jeffrey/pytorch/ucf101_two_stream/dic_video_label.pickle','rb') as f:
+        dic_video_label = pickle.load(f)
+    f.close()
     
-    #Data Loader
-    train_loader = DataLoader(
-        dataset=training_set, 
-        batch_size=BATCH_SIZE,
-        shuffle=True,
-        num_workers=8
-        )
-    
-    test_loader = DataLoader(
-        dataset=testing_set, 
-        batch_size=BATCH_SIZE, 
-        shuffle=False,
-        num_workers=8
-        )
-    
-    #Model
-    Model = models.resnet101(pretrained=True)
-    
-    #Replace fc1000 with fc101
-    num_ftrs = Model.fc.in_features
-    Model.fc = nn.Linear(num_ftrs, 101)
-    
-    #convert model to gpu
-    Model = Model.cuda()
-
-    #Loss function and optimizer
-    criterion = nn.CrossEntropyLoss().cuda()
-
-    optimizer = torch.optim.SGD(Model.parameters(), LR, momentum=0.9)
-    scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=0,verbose=True)
-
-    cudnn.benchmark = True
-
-    #Training & testing
-    best_prec1 = 0
-    for epoch in range(1,EPOCH+1):
-
-        print('****'*40)
-        print('Epoch:[{0}/{1}][training stage]'.format(epoch, EPOCH))
-        print('****'*40)
-        # train for one epoch
-        train(train_loader, Model, criterion, optimizer, epoch)
-
-        # evaluate on validation set
-        print('****'*40)
-        print('Epoch:[{0}/{1}][validation stage]'.format(epoch, EPOCH))
-        print('****'*40)
-
-        prec1, val_loss, dic_video_level_preds = validate(test_loader, Model, criterion, 0, L_test_keys,BATCH_SIZE)
+    correct = 0
+    video_level_preds = np.zeros((len(dic_video_level_preds),101))
+    video_level_labels = np.zeros(len(dic_video_level_preds))
+    ii=0
+    for line in sorted(dic_video_level_preds.keys()):
+        preds = dic_video_level_preds[line]
+        label = int(dic_video_label[line])-1
         
-        #Call lr_scheduler
-        scheduler.step(val_loss)
+        video_level_preds[ii,:] = preds
+        video_level_labels[ii] = label
+        ii+=1 
+        
+        if np.argmax(preds) == (label):
+            correct+=1
 
-        #Calculate Video level acc
-        top1,top5 = video_level_acc(dic_video_level_preds)
-
-        # remember best prec@1 and save checkpoint
-        is_best = prec1 > best_prec1
-        best_prec1 = max(prec1, best_prec1)
-        save_checkpoint({
-            'epoch': epoch + 1,
-            'arch': 'resnet101',
-            'state_dict': Model.state_dict(),
-            'best_prec1': best_prec1,
-            'optimizer' : optimizer.state_dict(),
-        }, is_best,dic_video_level_preds)
+    #top1 top5
+    video_level_labels = torch.from_numpy(video_level_labels).long()
+    video_level_preds = torch.from_numpy(video_level_preds).float()
     
-#other function 
+    top1,top5 = accuracy(video_level_preds, video_level_labels, topk=(1,5))     
+                    
+    top1 = float(top1.numpy())
+    top5 = float(top5.numpy())
+   
+    #record as csv file
+    filename = 'record/video_level_acc.csv'
+    column_names = ['Prec@1','Prec@5']
+    df =pd.DataFrame({'Prec@1':[top1],'Prec@5':[top5]})
 
+    if not os.path.isfile(filename):
+        df.to_csv(filename,index=False,columns=column_names)
+    else: # else it exists so append without writing the header
+        df.to_csv(filename,mode = 'a',header=False,index=False,columns=column_names)
+    
+    print(' * Video level Prec@1 {top1:.3f}, Video level Prec@5 {top5:.3f}'.format(top1=top1, top5=top5))
+    return top1,top5
+
+#other function 
 def accuracy(output, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
     maxk = max(topk)
